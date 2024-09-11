9b475539c4f0316b438d8a573a6803f0b7edab6b
Ayden.liu <w99wen@gmail.com>
2024-09-11 15:37:17 +0800

    Initial Commit

---
+++ TypeFlowView2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TypeFlowView2
+//
+//  Created by Ayden Liu on 2024/9/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
